f91cb6f998cbd87aafae4ca7171951ec45331ae7
njinco <xerusian@gmail.com>
2025-09-20 09:26:38 +0800

    Update install_dcs.sh

--- install_dcs.sh
+++ install_dcs.sh
@@ -51,11 +51,11 @@ while true; do
   curl -s -X POST "\$NC_URL" \\
     -H "xc-token: \$NC_API_KEY" \\
     -H "Content-Type: application/json" \\
-    -d '{
-      "hostname":"'"'\$DEVICE_HOSTNAME'"'",
-      "last_seen":"'"'\$TS'"'",
-      "ip":"'"'\$PUBIP'"'"
-    }' >/dev/null || true
+    -d "{
+      \\\"hostname\\\":\\\"\$DEVICE_HOSTNAME\\\",
+      \\\"last_seen\\\":\\\"\$TS\\\",
+      \\\"ip\\\":\\\"\$PUBIP\\\"
+    }" >/dev/null || true
 
   sleep "\$INTERVAL_SEC"
 done
